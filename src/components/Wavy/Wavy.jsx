0dbf10f941e596260a9d64ab854458f495b2fee4
Diomedes Git <diomedesgit@gmail.com>
2024-02-14 23:00:26 +0000

    memoising

--- src/components/Wavy/Wavy.jsx
+++ src/components/Wavy/Wavy.jsx
@@ -1,115 +1,110 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect, useRef, useMemo } from "react";
 import "./Wavy.css"
 
 const Wavy = () => {
   const canvasRef = useRef();
-  const [mousePos, setMousePos] = useState({ x: 0, y: 0 });
-  const [phase, setPhase] = useState(0.0001);
-  const [amplitude, setAmplitude] = useState(20);
+
+  const [phase, setPhase] = useState(0.00001);
+  const [amplitude, setAmplitude] = useState(10);
   const [frequency, setFrequency] = useState(0.000001);
+
+const [ampMaxReached, setAmpMaxReached] = useState(false);
+const [ampMinReached, setAmpMinReached] = useState(false);
+const [freqMaxReached, setFreqMaxReached] = useState(false);
+const [freqMinReached, setFreqMinReached] = useState(false);
+
+  const [mousePos, setMousePos] = useState({ x: 0, y: 0 });
   const [onClick, setOnClick] = useState(false);
 
 
-  const frequencyChange = 0.0001;
-  const amplitudeChange = 0.003;
-  const amplitudeMax = 20;
-  const amplitudeMin = 0.01;
+  const frequencyChange = 0.000005;
+  const amplitudeChange = 0.067;
+  const ampMax = 30;
+  const ampMin = 0.01;
+  const freqMax = 2;
+  const freqMin = 0.000001;
 
-
-  useEffect(() => {
-    const canvas = canvasRef.current;
-    const ctx = canvas.getContext("2d");
-
-    const drawWave = () => {
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-      ctx.beginPath();
-      ctx.moveTo(-1, canvas.height / 2);
-
-      const numPoints = 5000;
-      const stepSize = canvas.width / numPoints;
-
-      for (let x = 0; x < canvas.width; x += stepSize) {
-
-
-        // basic wave stuff, if you're curious:
+     // basic wave stuff, if you're curious:
         // y = Math.sin(x) * (frequency modifier)
         // y = (amplitude modifier) + Math.sin(x)
         // the more complicated looking stuff is basically doing variations 
-        // of that to change wave amplitude and frequency, and wavelngth is 
+        // of that to change wave amplitude and frequency, and wavelength is 
         // just inversely related to frequency. 
-        // canvas.height/2 places it in the middle of the defined canvas
-
-        // const ampModOne = onClick ? Math.sin(mousePos.x%(x - canvas.width)): 0;
-        // const ampModTwo = onClick ? Math.sin(mousePos.y%(x  - canvas.width)): 0;
-        // const ampModThree = onClick ? (mousePos.y * mousePos.x)%(x  - canvas.width)-phase: 0;
-
-        // Math.sin(ampModOne) + Math.sin(ampModTwo) + Math.random() * Math.sin(ampModThree)
+        // canvas.height/2 places it in the middle of the defined canvas, nudged slighly 
+        // to the side because canvas draws a wierd line at the edge of waves
 
 
-        const y = canvas.height / 2 + amplitude * Math.sin((x + phase) * frequency);
-        console.log(amplitude, frequency);
-        ctx.lineTo(x, y);
+
+const drawWave = useMemo (() => {
+return () => {
+  const canvas = canvasRef.current;
+  const ctx = canvas.getContext("2d");
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.beginPath();
+  ctx.moveTo(-1, canvas.height / 2);
+  const numPoints = 5000;
+  const stepSize = canvas.width / numPoints;
+  for (let x = 0; x < canvas.width; x += (stepSize)) {
+    const y = canvas.height / 2 + amplitude * Math.sin((x + phase) * frequency);
+    ctx.lineTo(x, y);
+  }
+  ctx.strokeStyle = "hsla(0, 0%, 0%, 0.99)";
+  ctx.stroke();
+};
+}, [amplitude, phase, frequency]);
+
+const updateWave = useMemo (() => {
+  return () => {
+    if (!ampMaxReached && amplitude < ampMax) {
+      setAmplitude((amplitude) => amplitude + amplitudeChange);
+      if (amplitude >= ampMax) {
+        setAmplitude(ampMax-amplitudeChange);
+        setAmpMaxReached(true);
+        setAmpMinReached(false);
       }
-      ctx.strokeStyle = "hsla(0, 0%, 0%, 0.99)";
-      ctx.stroke();
-    };
+    } else if (!ampMinReached && amplitude > ampMin) {
+      setAmplitude((amplitude) => amplitude - amplitudeChange);
+      if (amplitude <= ampMin) {
+        setAmplitude(ampMin+amplitudeChange);
+        setAmpMinReached(true);
+        setAmpMaxReached(false);
+      }
+    }
+    if (!freqMaxReached && frequency < freqMax) {
+      setFrequency((frequency) => frequency + frequencyChange);
+      if (frequency >= freqMax) {
+        setFrequency(freqMax);
+        setFreqMaxReached(true);
+        setFreqMinReached(false);
 
-    const animationID = requestAnimationFrame(drawWave);
-    return () => {
-      cancelAnimationFrame(animationID);
-    };
-  }, [phase, amplitude, frequency, mousePos, onClick]);
+      }
+    } else if (!freqMinReached && frequency > freqMin) {
+      setFrequency((frequency) => frequency - frequencyChange);
+      if (frequency <= freqMin) {
+        setFrequency(freqMin);
+        setFreqMinReached(true);
+        setFreqMaxReached(false);
 
-
+      }
+    }
+  };
+    
+  },[ampMaxReached, amplitude, ampMinReached, freqMaxReached, frequency, freqMinReached]);
 
   useEffect(() => {
-    let ampMaxReached = false;
-    let ampMinReached = false;
-    let freqMaxReached = false;
-    let freqMinReached = false;
-
-    const updateWave = () => {
-
-      if (!ampMaxReached) {
-        setAmplitude((amplitude) => amplitude + amplitudeChange);
-        if (amplitude >= amplitudeMax) {
-          setAmplitude(amplitudeMax);
-          ampMaxReached = true;
-        }
-      } else if (!ampMinReached) {
-        setAmplitude((amplitude) => amplitude - amplitudeChange);
-        if (amplitude >= amplitudeMin) {
-          setAmplitude(amplitudeMin);
-          ampMinReached = true;
-        }
-      } else {
-        let ampMaxReached = false;
-        let ampMinReached = false;
-
-      }
-
-      // if (frequency >= 3) {
-      //   freqIncrease = false;
-      // } else if (frequency <= 0.000001) {
-      //   freqIncrease = true;
-      // }
-
-      // if (freqMaxReached) {
-      //   setFrequency((frequency) => Math.min(frequency + frequencyChange, 3));
-      // } else if (freqMinReached) {
-      //   setFrequency((frequency) => Math.max(frequency - frequencyChange, 0.000001));
-      // }
-      setPhase((phase) => phase + Math.random() < 0.03 ? phase - 0.01 : phase + 0.01);
-
-
-    };
-
     const animationID = requestAnimationFrame(updateWave);
     return () => {
       cancelAnimationFrame(animationID);
     };
 
-  }, [amplitude, frequency]);
+  }, [updateWave]);
+
+  useEffect(() => {
+    const animationID = requestAnimationFrame(drawWave);
+    return () => {
+      cancelAnimationFrame(animationID);
+    };
+  }, [drawWave]);  
 
 
   //  mouse stuff, position and click events:
@@ -172,3 +167,27 @@ const Wavy = () => {
 };
 
 export default Wavy;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+          // const ampModOne = onClick ? Math.sin(mousePos.x%(x - canvas.width)): 0;
+        // const ampModTwo = onClick ? Math.sin(mousePos.y%(x  - canvas.width)): 0;
+        // const ampModThree = onClick ? (mousePos.y * mousePos.x)%(x  - canvas.width)-phase: 0;
+
+        // Math.sin(ampModOne) + Math.sin(ampModTwo) + Math.random() * Math.sin(ampModThree)

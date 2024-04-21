663228277f1d985f109d9ba758442903ab8c539e
Diomedes Git <diomedesgit@gmail.com>
2024-04-22 00:23:36 +0100

    number readout, not sure i love it

--- src/container/PageContainer.jsx
+++ src/container/PageContainer.jsx
@@ -1,23 +1,40 @@
 import NavBar from "../components/NavBar";
 import Menu from "../components/Menu/Menu";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const PageContainer = () => {
-  const [ modulation, setModulation] = useState(null);
-  const handleModSelect = (mod) => {
-    setModulation(mod);
-  }
+  
+  const [modBoolean, setModBoolean] = useState(false);
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 })
+
+  useEffect(() =>{
+    const updateMousePosition = (e) =>{
+      setMousePosition({ x: e.clientX, y: e.clientY });
+
+    };
+    document.addEventListener("mousemove", updateMousePosition);
+    return () =>{
+      document.removeEventListener("mousemove", updateMousePosition);
+    };
+  }, []);
+
 
   return (
     <>
       <div className="name">
         <h3>boagDev</h3>
       </div>
+    
       {/* <Menu onSelect={handleModSelect}/> */}
       <NavBar />
       <p className="text">
         theboag<br></br>[@]<br></br>gmail
       </p>
+      <div className="readout">
+        {/* AM: {(modBoolean ? 'on' : 'off')}<br></br> */}
+        X: {mousePosition.x} <br></br>
+        Y: {mousePosition.y}
+      </div>
     </>
   );
 };

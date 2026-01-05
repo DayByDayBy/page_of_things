75622cfc4736642ef03b96b0c454b1796fa77a3d
Diomedes Git <diomedesgit@gmail.com>
2026-01-05 00:55:28 +0000

    refactor: remove unnecessary fragment wrapper in index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -5,12 +5,9 @@ import './index.css';
 import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <>
-
-      <React.StrictMode>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </React.StrictMode>
-  </>
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );

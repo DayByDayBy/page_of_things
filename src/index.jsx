3bf37981afcef7ab4265f726a8a1fb5b04db393d
Diomedes Git <diomedesgit@gmail.com>
2026-01-12 02:58:12 +0000

    fix: clean up router future flags and JSX line breaks

--- src/index.jsx
+++ src/index.jsx
@@ -8,11 +8,7 @@ root.render(
   <React.StrictMode>
     <BrowserRouter future={{
       v7_startTransition: true,
-      v7_relativeSplatPath: true,
-      v7_fetcherPersist: true,
-      v7_normalizeFormMethod: true,
-      v7_partialHydration: true,
-      v7_skipActionErrorRevalidation: true
+      v7_relativeSplatPath: true
     }}>
       <App />
     </BrowserRouter>
